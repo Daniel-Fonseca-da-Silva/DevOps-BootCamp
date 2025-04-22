b4e142b9e23eab86d3ce35f68d964a7468be9e99
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-04-22 22:56:53 +0100

    feat: added security group

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "amb-prod" {
-  ami           = "ami-084568db4383264d4"
-  instance_type = "t2.micro"
-  key_name      = "Terraform"
+  ami                    = "ami-084568db4383264d4"
+  instance_type          = "t2.micro"
+  key_name               = "Terraform"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_http.id, aws_security_group.allow_egress.id]
 
   tags = {
     Name = "amb-prod"
