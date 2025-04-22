fc0a122e515885de1344789e01ef0d63f7d08486
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-04-23 00:53:45 +0100

    feat: created deploy of application

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "amb-prod" {
   instance_type          = "t2.micro"
   key_name               = "Terraform"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_http.id, aws_security_group.allow_egress.id]
+  user_data              = file("script.sh")
 
   tags = {
     Name = "amb-prod"
